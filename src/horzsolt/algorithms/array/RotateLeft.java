20185bf2cc4e483b6a1baa6ebdd7b47020d34d60
horzsolt <horzsolt2006@gmail.com>
2018-07-03 09:27:56 +0200

    array rotation added

--- src/horzsolt/algorithms/array/RotateLeft.java
+++ src/horzsolt/algorithms/array/RotateLeft.java
@@ -2,16 +2,16 @@ package horzsolt.algorithms.array;
 
 public class RotateLeft {
 
-    public static int[] rotLeft(int[] a, int d) {
+    public static int[] rotLeft(int[] array, int d) {
         // Because the constraints state d < n, we need not concern ourselves with shifting > n units.
-        int n = a.length;
+        int length = array.length;
 
         // Create new array for rotated elements:
-        int[] rotated = new int[n];
+        int[] rotated = new int[length];
 
         // Copy segments of shifted elements to rotated array:
-        System.arraycopy(a, d, rotated, 0, n - d);
-        System.arraycopy(a, 0, rotated, n - d, d);
+        System.arraycopy(array, d, rotated, 0, length - d);
+        System.arraycopy(array, 0, rotated, length - d, d);
 
         return rotated;
     }
--- src/horzsolt/algorithms/array/RotateLeftTest.java
+++ src/horzsolt/algorithms/array/RotateLeftTest.java
@@ -11,4 +11,19 @@ public class RotateLeftTest {
     void rotateLeftTest() {
         assertArrayEquals(new int[]{5,1,2,3,4}, RotateLeft.rotLeft(new int[]{1,2,3,4,5}, 4));
     }
+
+    @Test
+    void printTest() {
+        int arr1[] = { 0, 1, 2, 3, 4, 5 };
+        int arr2[] = { 5, 10, 20, 30, 40, 50 };
+
+        // copies an array from the specified source array
+        System.arraycopy(arr1, 0, arr2, 0, 2);
+        System.out.print("array2 = ");
+        System.out.print(arr2[0] + " ");
+        System.out.print(arr2[1] + " ");
+        System.out.print(arr2[2] + " ");
+        System.out.print(arr2[3] + " ");
+        System.out.print(arr2[4] + " ");
+    }
 }
